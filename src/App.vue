d3f9616b038b9bd15ad0e86d6207c7d907585fe6
johaol16 <johanna.n.olsson@hotmail.com>
2021-02-08 10:07:25 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Om Donify</router-link> |
+      <router-link to="/about">Logga in</router-link>
     </div>
     <router-view />
   </div>
