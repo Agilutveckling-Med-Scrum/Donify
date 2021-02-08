17b20e6b923a843b73ed0cc62d10e24e14a59611
emilodhe <emil.odhe@gmail.com>
2021-02-08 15:06:30 +0100

    ey

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <footer id="footer">
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-twitter"></a>
+      <a href="#" class="fa fa-instagram"></a>
+      <a href="#" class="fa fa-linkedin"></a>
+    </footer>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style scoped>
+#footer {
+  background-color: grey;
+  color: white;
+  height: 5vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  padding: 10px 10px 0px 10px;
+  bottom: 0;
+  width: 100%;
+}
+
+#footer a {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 1em;
+  color: white;
+}
+</style>
