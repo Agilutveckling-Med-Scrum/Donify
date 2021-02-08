714bf35422d32ec1264ca4dcf5e730f7301741d8
Lulin-S <70949875+Lulin-S@users.noreply.github.com>
2021-02-08 14:22:11 +0100

    created donationpage

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <HelloWorld></HelloWorld>
+    <Donationpage></Donationpage>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Donationpage from "@/components/donationpage.vue";
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+    Donationpage
+  }
+};
+</script>
