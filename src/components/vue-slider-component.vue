0d815604f340a3896d9596168edfddc7b133501f
Lulin-S <70949875+Lulin-S@users.noreply.github.com>
2021-02-08 13:26:25 +0100

    created donation page

---
+++ src/components/vue-slider-component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="slider">
+    <vue-slider v-model="value" />
+  </div>
+</template>
+
+<script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+export default {
+  components: {
+    VueSlider
+  },
+  data() {
+    return {
+      value: 0
+    };
+  }
+};
+</script>
+<style scoped>
+.slider {
+  width: 50%;
+  margin: 0 auto 00;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld></HelloWorld>
+    <Donationpage></Donationpage>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import Donationpage from "@/components/donationpage.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    Donationpage
   }
 };
 </script>
