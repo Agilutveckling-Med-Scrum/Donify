041e656e69baa702ca6527b0fc0b503d6461326a
Carel Williams <carel@live.se>
2021-02-10 11:33:18 +0100

    Buttons added

--- src/views/Walkthrough.vue
+++ src/views/Walkthrough.vue
@@ -1,21 +1,50 @@
 <template>
-  <div class="home">
-    <Questions />
-    <About />
+  <div class="walkthrough" style="background-color: grey">
+<h2>Välkommen till Donify!</h2>
+<p>Börja med att berätta vem du är</p>
+<div class="button-container" style="background-color:white">
+  <b-button variant="primary" class="mx-auto">Privat person</b-button>  
+  <b-button variant="primary" class="mx-auto">Företag</b-button>  
+  <b-button variant="primary" class="mx-auto">Organisation</b-button>  
+</div>
   </div>
 </template>
 
 <script>
-import Questions from '@/components/Questions.vue'
-import About from '@/views/About.vue'
 
 export default {
-	name: 'Home',
-	components: {
-		Questions,
-		About,
-	},
+    name: 'Home',
+    components: {
+
+    }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+.walkthrough{
+  position: absolute;
+   height: 100%;
+   width: 100%;
+}
+h2{
+margin-top: 22%;}
+
+.button-container{
+  margin: 0 auto;
+  height: auto;
+  position: relative;
+  width: 67%;
+
+}
+.button-container > button{
+  border-radius: 25px;
+  display: block;
+  padding: 18px;
+  margin:25px 0px;
+  width: 100%
+}
+
+
+
+</style>
