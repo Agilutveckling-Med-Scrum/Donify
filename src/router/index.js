9a5ae9d966bb30416849bde8c82ef82c20a89401
Carel Williams <carel@live.se>
2021-02-10 12:07:57 +0100

    changed router

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Walkthrough from '../views/Walkthrough.vue'
-import Questions from '../components/Questions.vue'
 import Preloader from '../components/PreloaderMobile.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-	{
-		path: '/',
-		name: 'Walkthrough',
-		component: Walkthrough,
-	},
-	{
-		path: '/questions',
-		name: 'Questions',
-		component: Questions,
-	},
-	{
-		path: '/preloader',
-		name: 'MobilePreloader',
-		component: Preloader,
-	},
+  {
+    path: '/',
+    name: 'Walkthrough',
+    component: Walkthrough,
+  },
+  {
+    path: '/preloader',
+    name: 'MobilePreloader',
+    component: Preloader,
+  },
 ]
 
 const router = new VueRouter({
-	routes,
+  routes,
 })
 
 export default router
