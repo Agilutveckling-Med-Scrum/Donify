a9b3f5e6988b48ef373b85a5db61a13ba92a197e
johaol16 <johanna.n.olsson@hotmail.com>
2021-02-12 10:32:01 +0100

    add

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
-/* eslint-disable indent */
-=======
+
 /* eslint-disable*/
->>>>>>> da259e3e33d40938e190c62082de3c3c159a604e
+
 
 import { register } from 'register-service-worker'
 
