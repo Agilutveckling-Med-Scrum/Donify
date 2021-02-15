1bf01e6922c609a56acc3d0b9d2bdf92cb1015be
Lulin-S <70949875+Lulin-S@users.noreply.github.com>
2021-02-15 13:05:42 +0100

    add basic styles in three donationpages and add a method in aboutdonifypage

--- src/components/donationpg1.vue
+++ src/components/donationpg1.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="donationPage1">
-        <p>Du har valt:</p>
+        <p>Du har valt: <button class="vald">WWF</button></p>
         <p>Hur mycket vill du donera?</p>
         <VueSlider />
         <select v-model="selected">
@@ -74,19 +74,31 @@ export default {
 </script>
 
 <style scoped>
-template {
-    background-color: #f1f4f4;
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+.donationPage1 {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 800;
+    padding: 20px;
 }
-button {
-    margin-left: 10px;
+.vald {
+    height: 80px;
+    width: 80px;
+    background-color: #bc6ecc;
+    border: transparent;
+    border-radius: 100px;
 }
-
 .upbuttons {
     display: flex;
     justify-content: space-between;
 }
+.buttons {
+    display: flex;
+    flex-direction: column;
+    height: 240px;
+    justify-content: space-between;
+}
 .downbutton {
-    margin-top: 200px;
+    margin: 0 auto;
 }
 p {
     margin-top: 26px;
@@ -97,13 +109,17 @@ select {
 }
 #btn {
     font-size: 14px;
+    height: 40px;
+    width: 100px;
+    color: rgb(255, 255, 255);
     border: transparent;
-    background-color: #cb99c5;
-    box-shadow: 0px 2px 0px #ad83a8;
+    border-radius: 100px;
+    background-color: #8a2be2;
+    box-shadow: 0px 2px 0px #6f01d6;
 }
 #btn:focus {
     box-shadow: 0 0 #ba8cb5;
-    background-color: #b663ab;
+    background-color: #9d55e0;
 }
 
 @media only screen and (min-device-width: 375px) and (max-device-width: 812px) {
