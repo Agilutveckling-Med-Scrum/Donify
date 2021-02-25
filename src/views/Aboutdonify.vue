cb42d46286d515e7dc219f6f7dadc02f0b7d3074
johaol16 <johanna.n.olsson@hotmail.com>
2021-02-25 11:05:40 +0100

    merge conflicts

--- src/views/Aboutdonify.vue
+++ src/views/Aboutdonify.vue
@@ -1,69 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="aboutdonify">
-        <div class="container">
-            <h2>OmDonify</h2>
-            <section class="section">
-                <div class="container-fliud">
-                   <div id="btn">
-                    <button
-                        type="button"
-                        class="btn btn-primary btn-lg"
-                        @click="Todonera">Donera nu
-                    </button>
-                    </div>
-                </div>
-            </section>
-            <div class="row">
-                <div class="col-lg-4">
-                    <h2>Nyheter</h2>
-                    <p>
-                        Donec sed odio dui. Etiam porta sem malesuada magna
-                        mollis euismod. .
-                    </p>
-                    <p>
-                        <a class="btn btn-secondary" href="#" role="button"
-                            >View details »</a
-                        >
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h2>Updates</h2>
-                    <p>
-                        Donec sed odio dui. Etiam porta sem malesuada magna
-                        mollis euismod. cursus magna.
-                    </p>
-                    <p>
-                        <a class="btn btn-secondary" href="#" role="button"
-                            >View details »</a
-                        >
-                    </p>
-                </div>
-                <div class="col-lg-4">
-                    <h4>Toplista</h4>
-                    <select v-model="topLista">
-                        <option
-                            v-for="popular in toplist"
-                            v-bind:key="popular"
-                            :value="popular.key"
-                            >{{ popular.name }}</option
-                        >
-                    </select>
-
-                    <h2>Notifications</h2>
-                    <p>
-                        Donec sed odio dui. Etiam porta sem malesuada magna
-                        mollis euismod.Praesent commodo cursus magna.
-                    </p>
-                    <p>
-                        <a class="btn btn-secondary" href="#" role="button"
-                            >View details »</a
-                        >
-                    </p>
-                </div>
-            </div>
-        </div>
-=======
   <div class="about">
     <h2 class="heading">Om Donify</h2>
     <div class="container">
@@ -86,7 +21,6 @@
     </div>
     <div class="circle2">
       <p class="circletx2">Donify-kontor</p>
->>>>>>> 429637a34da74f0675b579d5919b7e42d326801b
     </div>
     <div class="circle3">
       <p class="circletx3">Ej verksamma</p>
@@ -94,41 +28,7 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-<script>
-export default {
-    name: 'Aboutdonify',
-    data: function() {
-        return {
-            topLista: [],
-            toplist: [
-                { key: 1, name: 'WHO' },
-                { key: 2, name: 'RäddaBarnen' },
-                { key: 3, name: 'Unicef' },
-                { key: 4, name: 'Amnesty International' },
-                { key: 5, name: '' },
-                { key: 6, name: '' },
-                { key: 7, name: '' }
-            ]
-        }
-    },
-    // add method to click to donationpage
-    methods: {
-        Todonera() {
-            this.$router.push({
-                name: 'Donationpage1'
-            })
-        }
-    }
-}
-</script>
-<style scoped>
-.aboutdonify {
-    display: flex;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-        Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    flex-direction: column;
-=======
+
 <style scoped>
 /* Raleway */
 @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@600&display=swap');
@@ -178,7 +78,6 @@ p {
   100% {
     width: 14ch;
   }
->>>>>>> 429637a34da74f0675b579d5919b7e42d326801b
 }
 h2 {
   font-family: 'Raleway', san-serif;
@@ -188,30 +87,7 @@ h2 {
   justify-content: center;
   color:rgb(53, 52, 52);
 }
-<<<<<<< HEAD
 
-
-
-
-.btn-group {
-
-    min-width: 160px;
-    height: 40px;
-    position: relative;
-    align-items: center;
-    margin: 10px 1px;
-    display: inline-block;
-    vertical-align: middle;
-}
-
-
-
-body {
-    background-color: #666666;
-    min-height: 100vh;
-    display: flex;
-    font-weight: 400;
-=======
 .heading {
   text-align: center;
 }
@@ -267,6 +143,5 @@ body {
 }
 .circletx3 {
   width: 250px;
->>>>>>> 429637a34da74f0675b579d5919b7e42d326801b
 }
 </style>
